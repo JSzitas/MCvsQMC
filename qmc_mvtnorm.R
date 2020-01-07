efef0b8fac2dcdfcd566f86929ddfb80e22640bf
Juraj Szitás <JSzitas@users.noreply.github.com>
2020-01-07 10:45:16 +0100

    some minor cleanup in code structure

--- qmc_mvtnorm.R
+++ qmc_mvtnorm.R
@@ -1,41 +1,41 @@
 # quasi random multivariate normal distribution
 
-rmvnorm_qmc <- function(n, mean = rep(0, nrow(sigma)), sigma = diag(length(mean)),
-                    method=c("eigen", "svd", "chol"), pre0.9_9994 = FALSE, 
-                    time_rng = FALSE, reinit = FALSE)
+rmvnorm_qmc <- function( n,
+                         mean = rep(0, nrow(sigma)),
+                         sigma = diag(length(mean)),
+                         method = "eigen", # other options include chol, svd
+                         time_rng = FALSE,
+                         reinit = FALSE )
 {
   
   checkmate::assert_true(isSymmetric(sigma, tol = sqrt(.Machine$double.eps),
                                      check.attributes = FALSE))
   checkmate::assert_set_equal(length(mean), nrow(sigma))
   
-  method <- match.arg(method)
-  
-  R <- if(method == "eigen") {
-    ev <- eigen(sigma, symmetric = TRUE)
-    if (!all(ev$values >= -sqrt(.Machine$double.eps) * abs(ev$values[1]))){
+
+  if(method == "eigen"){
+  res <- eigen(sigma, symmetric = TRUE)
+    if (!all(res$values >= -sqrt(.Machine$double.eps) * abs(res$values[1]))){
       warning("sigma is numerically not positive semidefinite")
-    }
-    ## ev$vectors %*% diag(sqrt(ev$values), length(ev$values)) %*% t(ev$vectors)
-    ## faster for large  nrow(sigma):
-    t(ev$vectors %*% (t(ev$vectors) * sqrt(pmax(ev$values, 0))))
+       }
+  res <- t(res$vectors %*% (t(res$vectors) * sqrt(pmax(res$values, 0))))
   }
   else if(method == "svd"){
-    s. <- svd(sigma)
-    if (!all(s.$d >= -sqrt(.Machine$double.eps) * abs(s.$d[1]))){
+    res <- svd(sigma)
+    if (!all(res$d >= -sqrt(.Machine$double.eps) * abs(res$d[1]))){
       warning("sigma is numerically not positive semidefinite")
     }
-    t(s.$v %*% (t(s.$u) * sqrt(pmax(s.$d, 0))))
+    res <- t(res$v %*% (t(res$u) * sqrt(pmax(res$d, 0))))
   }
   else if(method == "chol"){
-    R <- chol(sigma, pivot = TRUE)
-    R[, order(attr(R, "pivot"))]
+    res <- chol(sigma, pivot = TRUE)
+    res[, order(attr(res, "pivot"))]
   }
   
-  retval <- matrix(randtoolbox::halton(n, dim = ncol(sigma), normal = TRUE,
-                                       usetime = time_rng, init = reinit),
-                   nrow = n, byrow = !pre0.9_9994) %*%  R
-  retval <- sweep(retval, 2, mean, "+")
-  colnames(retval) <- names(mean)
-  return(retval)
+  res <- matrix(randtoolbox::halton( n, dim = ncol(sigma), normal = TRUE,
+                                     usetime = time_rng, init = reinit),
+                   nrow = n ) %*%  res
+  res <- sweep(res, 2, mean, "+")
+  colnames(res) <- names(mean)
+  return(res)
 }
--- qmc_rgamma.R
+++ qmc_rgamma.R
@@ -9,8 +9,6 @@ rgamma_qmc <- function( shape, scale ){
     c <- 1/sqrt(9*d)
     flag <- 1
     while(flag == 1){
-      # Z <- randtoolbox::halton( n = 1, dim = 1, usetime = TRUE)
-      # Z <- dnorm(Z)
       Z <- randtoolbox::halton( n = 1, dim = 1, usetime = TRUE, normal = TRUE)
       if( Z > (-1/c) ){
         V <- (1 + c * Z)^3
@@ -31,43 +29,49 @@ rgamma_qmc <- function( shape, scale ){
 rgamma_qmc_2 <- function(n = 1, shape, scale , reinit = FALSE, time_rng = FALSE){
   res_vec <- rep(NA, n)
   Z_vec <- randtoolbox::halton( n, dim = 1, normal = TRUE,
-                                usetime = time_rng, init = reinit) #usetime = TRUE,
+                                usetime = time_rng, init = reinit)
   U_vec <- randtoolbox::halton( n, dim = 1,
-                                usetime = time_rng, init = reinit ) #usetime = TRUE,
+                                usetime = time_rng, init = reinit )
   final_x_mult <- randtoolbox::halton( n, dim = 1,
                                        usetime = time_rng,
-                                       init = reinit ) #usetime = TRUE,
+                                       init = reinit )
   alpha <- shape
   theta <- scale
-for(i in 1:n){
-  if(alpha > 1){
-    d<- alpha - 1/3
-    c <- 1/sqrt(9*d)
-    flag <- 1
-    iter_counter <- 0
-    while(flag == 1){
-      Z <- Z_vec[i]
-      iter_counter <- iter_counter + 1
-      if( Z > (-1/c) ){
-        V <- (1 + c * Z)^3
-        U <- U_vec[i]
-        flag <- log(U) > (((0.5*Z)^2) +d -(d*V) +(d*log(V))) 
+  for(i in 1:n){
+    if(alpha > 1)
+      {
+      d<- alpha - 1/3
+      c <- 1/sqrt(9*d)
+      flag <- 1
+      iter_counter <- 0
+        while(flag == 1)
+        {
+          Z <- Z_vec[i]
+          iter_counter <- iter_counter + 1
+            if( Z > (-1/c) )
+            {
+            V <- (1 + c * Z)^3
+            U <- U_vec[i]
+            flag <- log(U) > (((0.5*Z)^2) +d -(d*V) +(d*log(V))) 
+            }
+              if(iter_counter > 10000)
+              {
+                Z <- randtoolbox::halton( n, dim = 1, normal = TRUE)
+              }
+              if(iter_counter > 100000)
+              {
+                break;
+              }
+        }
+        x <- d * (V/theta)
       }
-      if(iter_counter > 10000){
-        Z <- randtoolbox::halton( n, dim = 1, normal = TRUE)
+      else
+      {
+        x <-  rgamma_qmc(alpha+1,theta)
+        x <- x*final_x_mult[i]^(1/alpha)
       }
-      if(iter_counter > 100000){
-        break;
-      }
-    }
-    x <- d * (V/theta)
-  }
-  else{
-    x <-  rgamma_qmc(alpha+1,theta)
-    x <- x*final_x_mult[i]^(1/alpha)
-  }
-  res_vec[i] <- x
-}  
+    res_vec[i] <- x
+  }  
 return(res_vec)
 }
 
